6bc00b2c557071442150947f3dd51d1737727f14
tonmoyOrg <tonmoy15-5200@diu.edu.bd>
2026-02-26 14:14:58 +0600

    ok

--- src/api/phoneNumbers.js
+++ src/api/phoneNumbers.js
@@ -27,18 +27,15 @@ export const updatePhoneNumber = async ({ id, data }) => {
 
 export const deletePhoneNumber = async (id) => {
     const { data } = await axiosInstance.delete(`/phone-numbers/${id}`);
-    console.log(data);
     return data;
 };
 
 export const bulkDeletePhoneNumbers = async (ids) => {
     const { data } = await axiosInstance.delete('/phone-numbers/bulk', { data: { ids } });
-    console.log(data);
     return data;
 };
 
 export const bulkUpdatePhoneNumberStatus = async (ids, status) => {
     const { data } = await axiosInstance.patch('/phone-numbers/bulk/status', { ids, is_active: status });
-    console.log(data);
     return data;
 };
